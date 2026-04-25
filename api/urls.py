9d2a2b08108db1ff31fde65e09c3b045dbbc0e20
Denis <denis.Guselnikov2007@mail.ru>
2026-04-25 21:18:34 +0700

    5

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import PostViewSet, GroupViewSet, CommentViewSet, FollowViewSet
-
+# /
 router = DefaultRouter()
 router.register(r'posts', PostViewSet, basename='post')
 router.register(r'groups', GroupViewSet, basename='group')
