0fb37fa82078becf909d1245aa5790d4fb7195cf
Denis <denis.Guselnikov2007@mail.ru>
2026-04-25 20:56:46 +0700

    1

---
+++ api/serializers.py
@@ -0,0 +1,44 @@
+from rest_framework import serializers
+from rest_framework.relations import SlugRelatedField
+
+
+from posts.models import Comment, Post, Follow, User, Group
+
+class GroupSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Group
+        fields = ('id', 'title', 'slug', 'description')
+
+class PostSerializer(serializers.ModelSerializer):
+    author = SlugRelatedField(slug_field='username', read_only=True)
+
+    class Meta:
+        fields = '__all__'
+        model = Post
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    author = serializers.SlugRelatedField(
+        read_only=True, slug_field='username'
+    )
+
+    class Meta:
+        fields = '__all__'
+        model = Comment
+
+class FollowSerializer(serializers.ModelSerializer):
+    user = serializers.SlugRelatedField(read_only=True, slug_field='username')
+    following = serializers.SlugRelatedField(
+        queryset=User.objects.all(),
+        slug_field='username'
+    )
+    
+    class Meta:
+        model = Follow
+        fields = ('user', 'following')
+        
+    def validate_following(self, value):
+        request = self.context.get('request')
+        if request.user == value:
+            raise serializers.ValidationError("Нельзя подписаться на самого себя!")
+        return value
---
+++ api/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import PostViewSet, GroupViewSet, CommentViewSet, FollowViewSet
+
+router = DefaultRouter()
+router.register(r'posts', PostViewSet, basename='post')
+router.register(r'groups', GroupViewSet, basename='group')
+router.register(r'follow', FollowViewSet, basename='follow')
+
+urlpatterns = [
+    path('v1/', include('djoser.urls')),
+    path('v1/', include('djoser.urls.jwt')),
+    path('v1/', include(router.urls)),
+    path('v1/posts/<int:post_id>/comments/', CommentViewSet.as_view({'get': 'list', 'post': 'create'}), name='comment-list'),
+    path('v1/posts/<int:post_id>/comments/<int:pk>/', CommentViewSet.as_view({'get': 'retrieve', 'put': 'update', 'patch': 'partial_update', 'delete': 'destroy'}), name='comment-detail'),
+]
+
